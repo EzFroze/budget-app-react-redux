341a67c36c72e4de44c5524b3562e78a2e8e9f10
EzFroze <sultanbekov-01@yandex.ru>
2020-11-12 19:42:32 +0300

    Форматирование кода

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import Header from './components/Header';
-import Balance from './components/Balance';
-import AddTransactiom from './components/AddTransaction';
-import IncomeList from './components/IncomeList';
-import ExpenseList from './components/ExpenseList';
+import React from "react"
+import './App.css'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import AddTransaction from './components/AddTransaction'
+import IncomeList from './components/IncomeList'
+import ExpenseList from './components/ExpenseList'
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <div className="app-wrapper">
         <Header/>
         <Balance/>
-        <AddTransactiom/>
+        <AddTransaction/>
         <IncomeList/>
         <ExpenseList/>
       </div>
