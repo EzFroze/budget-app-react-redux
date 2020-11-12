7762f398039372e7a8a6bda87d764cbaebbe2061
EzFroze <sultanbekov-01@yandex.ru>
2020-11-12 19:42:14 +0300

    Удалил ненужные файлы после перехода на redux toolkit

--- src/actions/actionsTypes.js
+++
@@ -1,6 +0,0 @@
-export const INCREMENT = "INCREMENT"
-export const DECREMENT = "DECREMENT"
-export const INCOME = "INCOME"
-export const EXPENSE = "EXPENSE"
-export const DELETEINCOME = "DELETEINCOME"
-export const DELETEEXPENSE = "DELETEEXPENSE"
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import Counter from './Couner';
-import { combineReducers } from 'redux';
-
-const allReducers = combineReducers({
-    Counter
-})
-
-export default allReducers;
