fd5a2ac6ac04ce43316b09d55b356d1cad65c03c
EzFroze <sultanbekov-01@yandex.ru>
2020-11-12 19:44:09 +0300

    Переход на redux toolkit

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { deleteExpense } from '../actions'
+import {useSelector, useDispatch} from 'react-redux'
+import {deleteExpense, selectExpenseHistory} from "../store/Budget"
 
-const ExpenseList = () => {
-    const historyCounter = useSelector(state => state.Counter);
-    const dispatch = useDispatch()
+function ExpenseList() {
+  const expenseHistory = useSelector(selectExpenseHistory)
+  const dispatch = useDispatch()
 
-    return (
-       <div className="transactions transactions-expense">
-            <h2>Transaction History</h2>
-            <ul className="transaction-list">
-            {historyCounter.expense.map(el => (
-                    <li className="transaction" key={el.id}>
-                      <span className="transaction-text">{el.name}</span>
-                      <span className="transaction-amount">${el.amount}</span>
-                      <button className="delete-btn" onClick={() => dispatch(deleteExpense(el.id, el.amount))}>
-                          <i className="fas fa-trash"></i>
-                      </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const deleteExpenseBtn = (id, amount) => dispatch(deleteExpense({id, amount}))
+
+  return (
+    <div className="transactions transactions-expense">
+      <h2>Transaction History</h2>
+      <ul className="transaction-list">
+        {expenseHistory.map(el => (
+          <li className="transaction" key={el.id}>
+            <span className="transaction-text">{el.name}</span>
+            <span className="transaction-amount">${el.amount}</span>
+            <button className="delete-btn" onClick={() => deleteExpenseBtn(el.id, el.amount)}>
+              <i className="fas fa-trash"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default ExpenseList
